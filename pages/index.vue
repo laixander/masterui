9d0fc02fb2f6566451b06a6c5e5177e6d9dd4893
Laixander Naguit <laixander@gmail.com>
2024-11-26 16:08:29 +0800

    Updates

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="antialiased bg-white dark:bg-black dark:text-white flex flex-col items-center justify-center min-h-screen place-content-center sm:text-base text-black text-sm">
+
+        <UContainer class="w-full mt-8 p-4 border border-gray-200 rounded-lg shadow-sm bg-white">
+        <div class="text-gray-600 flex items-center gap-3">
+            <UIcon name="i-heroicons-home" class="w-5 h-5" /> Hello World
+        </div>
+    </UContainer>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    bodyAttrs: {
+        class: ['bg-gray-50']
+    },
+});
+</script>
+
+<style scoped>
+
+</style>
